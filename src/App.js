19780beabcc47e760e214dd2b5183a3d995eb1fa
Thiha-aung-eng <thihaaung@ucstgi.edu.mm>
2022-06-29 22:45:06 +0630

    Api create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Create from './components/Create'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-     API TESTING!!
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+      <Create />
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
